a6e3831ab3d9b7200991a05aa3d3e60e2de2aa60
Shreedhar Hegde <akshitaagarwal@Akshitas-MacBook-Pro.local>
2020-12-02 19:46:15 +0530

    changed db url and added few comments

--- app.js
+++ app.js
@@ -1,45 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Middleware for parsing json
 app.use(express.json());
 
 // Import Rotes
-const postRoute = require('./routes/posts');
+const postRoute = require("./routes/posts");
 
-app.use('/posts', postRoute);
+app.use("/posts", postRoute);
 
 // Connect to db (Atlas)
 // Enter your mongodb Atlas user name and password in their respective field below
-const dbURI = 'mongodb+srv://<dbuser>:<dbpassword>@nodetuts.wmpmq.mongodb.net/node-tuts?retryWrites=true&w=majority';
-mongoose.connect(dbURI, {
-    useNewUrlParser: true, useUnifiedTopology: true},
-    () => {
-    console.log('connected to db');
-});
-
-
+const dbURI = "mongodb://localhost:27017/local";
+mongoose.connect(
+  dbURI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("connected to db");
+  }
+);
 
 // Create Routes
-app.get('/', (req, res) => {
-    res.send('Home');
+app.get("/", (req, res) => {
+  console.log("reached / route");
+  res.send("Home");
 });
 
-
-
 // Listening to server
-app.listen(3000);
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
--- routes/posts.js
+++ routes/posts.js
@@ -1,65 +1,68 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Post = require('../models/post');
+const Post = require("../models/post");
 
 // Get all posts
-router.get('/', async (req, res) => {
-    try {
-        const posts = await Post.find();
-        res.json(posts);
-    } catch (err) {
-        res.json({ message: err});
-    }  
+router.get("/", async (req, res) => {
+  //have this to check if request is reaching the route
+  console.log("reached get posts route");
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 // Get a specific post
-router.get('/:postId', async (req, res) => {
-    try {
-        const specificPost = await Post.find({ _id : req.params.postId });
-        res.json(specificPost);
-    } catch (err) {
-        res.json({ message: err});
-    }  
+router.get("/:postId", async (req, res) => {
+  console.log("reached get post by id route");
+  try {
+    const specificPost = await Post.find({ _id: req.params.postId });
+    res.json(specificPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 // Submit a post
-router.post('/', async (req, res) => {
-    const post = new Post({
-        title: req.body.title,
-        description: req.body.description
-    });
-    try {
-        const savedPost = await post.save();
-        res.send(savedPost);
-    } catch (err) {
-        res.json({ message: err});
-    }  
+router.post("/", async (req, res) => {
+  console.log("reached create post route");
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+  });
+  try {
+    const savedPost = await post.save();
+    res.send(savedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 // Delete a post
-router.get('/:postId', async (req, res) => {
-    try {
-        const removedPost = await Post.remove({ _id : req.params.postId });
-        res.json(removedPost);
-    } catch (err) {
-        res.json({ message: err});
-    }  
+router.get("/:postId", async (req, res) => {
+  console.log("reached delete post route");
+  try {
+    const removedPost = await Post.remove({ _id: req.params.postId });
+    res.json(removedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 // Update post
-router.patch('/:postId', async (req, res) => {
-    try {
-        const updatedPost = await Post.updateOne(
-            { _id : req.params.postId },
-            { $set: { title: req.body.title}});
-        res.json(updatedPost);
-    } catch (err) {
-        res.json({ message: err});
-    }  
+router.patch("/:postId", async (req, res) => {
+  console.log("reached patch post route");
+  try {
+    const updatedPost = await Post.updateOne(
+      { _id: req.params.postId },
+      { $set: { title: req.body.title } }
+    );
+    res.json(updatedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 module.exports = router;
-
-
-
-
